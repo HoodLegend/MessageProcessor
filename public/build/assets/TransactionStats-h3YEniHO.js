ef5189f45a5a269db7db4e3c64db44aaf31c9c82
Mofya Ndabala <61332272+HoodLegend@users.noreply.github.com>
2025-07-30 15:54:51 +0200

    Completed parsing and displaying transactions

--- public/build/assets/TransactionStats-h3YEniHO.js
+++ public/build/assets/TransactionStats-h3YEniHO.js
@@ -1,4 +1,4 @@
-var dn=Object.defineProperty;var fn=(i,t,e)=>t in i?dn(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var O=(i,t,e)=>fn(i,typeof t!="symbol"?t+"":t,e);import{_ as un}from"./AuthenticatedLayout-RK4wmAbS.js";import{l as gi,y as gn,c as pn,o as Re,w as mn,a as bn,b as R,u as _n,g as xn,f as pi,t as Ot}from"./app-D66tDBW9.js";import"./ApplicationLogo-DFh0JLSA.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./DigitalClock-DMrhY9ig.js";/*!
+var dn=Object.defineProperty;var fn=(i,t,e)=>t in i?dn(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var O=(i,t,e)=>fn(i,typeof t!="symbol"?t+"":t,e);import{_ as un}from"./AuthenticatedLayout-BQNvqG-O.js";import{l as gi,y as gn,c as pn,o as Re,w as mn,a as bn,b as R,u as _n,g as xn,f as pi,t as Ot}from"./app--2QCEKys.js";import"./ApplicationLogo-Ca7v346q.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./DigitalClock-oCMnIqhV.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
