39586ff24be0f27dda44ec5552e54db3a75d0ea0
Mofya Ndabala <61332272+HoodLegend@users.noreply.github.com>
2025-07-30 11:43:42 +0200

    fixed the parsing issue and added table to show results

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -220,7 +220,7 @@ public function getCsvData(Request $request)
 {
     try {
         // Get the latest CSV file or specific file
-        $directory = 'exports';
+        $directory = 'app/private/exports';
         $files = Storage::files($directory);
 
         // Filter for CSV files and get the latest one
